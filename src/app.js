95025698ea9f0a3cd83aff8075c151f1b28f6b8f
Adnan Usman <adnantu@hotmail.com>
2019-03-07 20:41:05 -0800

    added handlebars to the environment

--- src/app.js
+++ src/app.js
@@ -3,9 +3,35 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+// setup paths for express configuration
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// setup express static folder to serve from
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Homepage'
+  });
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Us'
+  });
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Section'
+  });
+})
 
 app.get('/weather', (req, res) => {
   res.send([{
