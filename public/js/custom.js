89d9866ee07ab532bf3fddb400f1708aba304a91
Adnan Usman <adnantu@hotmail.com>
2019-03-08 19:55:55 -0800

    Added form for user input, styling. Application returns weather data now.

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,7 +1,38 @@
-fetch('http://localhost:3000/weather?address=Google').then((response) => {
-  if(response) {
-    return response.json();
+class WeatherFinder {
+  constructor() {
+    this.setupEventListeners();
   }
-}).then((data) => {
-  console.log(data);
-})
+
+  setupEventListeners = () => {
+    const form = document.getElementById('weather-finder');
+    
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();        
+      const location = document.getElementById('location');
+
+      this.getWeather(location.value);
+    });
+  }
+
+  getWeather = (location) => {
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+      if(response) {
+        return response.json();
+      }
+    }).then((data) => {
+      this.renderData(data);
+    })
+  }
+
+  renderData = (data) => {
+    const resultsDiv = document.getElementById('results');
+
+    resultsDiv.innerHTML = `
+      <p>Forecast: ${data.forecast}</p>
+      <p>Temperature: ${data.temperature}</p>
+      <p>Chances of rain: ${data.rainProbability}</p>
+    `;
+  }
+}
+
+const weatherFinder =  new WeatherFinder;
